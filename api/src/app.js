39bb7c654b7fbfa7d5be61724afe09fb3e8bb6fa
Dm Hl <dmitrogluhovcov@gmail.com>
2021-12-07 23:21:10 +0200

    add dependencies

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.post('/users', (req, res) => {
+    res.send('Hello from users');
+});
+app.listen(process.env.APP_PORT);
