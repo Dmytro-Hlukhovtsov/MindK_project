670f93e5c0fc0519bd22247355115b2a72d3ea87
Dm Hl <dmitrogluhovcov@gmail.com>
2021-12-31 00:56:48 +0200

    update dependencies

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "123456",
+  port: 5432,
 });
-app.post('/users', (req, res) => {
-    res.send('Hello from users');
+
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM uset;", (error, results) => {
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).json(results.rows);
+  });
 });
+
+console.log(process.env.APP_PORT);
 app.listen(process.env.APP_PORT);
+
+// docker-compose --env-file .env up -d
